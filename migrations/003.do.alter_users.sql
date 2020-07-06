b37298e45715efe85f02890162c8c8e2b649a8a9
Allegra Pusateri <allegrapusateri@gmail.com>
2020-07-05 20:43:09 -0500

    Updated tables

---
+++ migrations/003.do.alter_users.sql
@@ -0,0 +1,199 @@
+ALTER TABLE users
+    ADD COLUMN
+        yum_1 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_2 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_3 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_4 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_5 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_6 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_7 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_8 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_9 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_10 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_11 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_12 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_13 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_14 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_15 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_16 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_17 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_18 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_19 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_20 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_21 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_22 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_23 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_24 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_25 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_26 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_27 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_28 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_29 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_30 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_31 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_32 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_33 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_34 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_35 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_36 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_37 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_38 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_39 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_40 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_41 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_42 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_43 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_44 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_45 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_46 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_47 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_48 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_49 INTEGER;
+
+ALTER TABLE users
+    ADD COLUMN
+        yum_50 INTEGER;
